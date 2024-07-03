eab9ba4380edd59779e703ae5a11c642b76eea57
Giacomo Mancin <iemes514@gmail.com>
2024-07-03 20:35:41 +0200

    Add files via upload

---
+++ Core/Inc/LCD.h
@@ -0,0 +1,20 @@
+/*
+ * LCD.h
+ *
+ *  Created on: Jun 10, 2023
+ *      Author: Giacomo
+ */
+
+#ifndef INC_LCD_H_
+#define INC_LCD_H_
+
+void LCD_STROBE(void);
+void ldc_write_cmd(unsigned char);
+void ldc_write_data(unsigned char);
+void lcd_clear(void);
+void lcd_puts(const char * s);
+void lcd_putch(char c);
+void lcd_goto(int,int);
+void lcd_init(void);
+
+#endif /* INC_LCD_H_ */
